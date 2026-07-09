94d8a0ef78e9d4fafe0fd180c02b6fc782b72c35
itsmmunoz <estivenyeizon@gmail.com>
2026-07-08 21:03:44 -0500

    feat(gnome): add extensions config and workspace keybindings

--- home/mmunoz/dconf.nix
+++ home/mmunoz/dconf.nix
@@ -34,5 +34,41 @@
       command = "gnome-characters";
       name = "Open Characters";
     };
+
+    "org/gnome/shell" = {
+      enabled-extensions = [
+        "user-theme@gnome-shell-extensions.gcampax.github.com"
+        "blur-my-shell@aunetx"
+        "clipboard-indicator@tudmotu.com"
+      ];
+    };
+
+    "org/gnome/shell/extensions/clipboard-indicator" = {
+      toggle-menu = [ "<Super>v" ];
+    };
+
+    "org/gnome/desktop/wm/keybindings" = {
+      switch-to-workspace-1 = [ "<Super>1" ];
+      switch-to-workspace-2 = [ "<Super>2" ];
+      switch-to-workspace-3 = [ "<Super>3" ];
+      switch-to-workspace-4 = [ "<Super>4" ];
+      switch-to-workspace-5 = [ "<Super>5" ];
+      switch-to-workspace-6 = [ "<Super>6" ];
+      switch-to-workspace-7 = [ "<Super>7" ];
+      switch-to-workspace-8 = [ "<Super>8" ];
+      switch-to-workspace-9 = [ "<Super>9" ];
+    };
+
+    "org/gnome/shell/keybindings" = {
+      switch-to-application-1 = [ ];
+      switch-to-application-2 = [ ];
+      switch-to-application-3 = [ ];
+      switch-to-application-4 = [ ];
+      switch-to-application-5 = [ ];
+      switch-to-application-6 = [ ];
+      switch-to-application-7 = [ ];
+      switch-to-application-8 = [ ];
+      switch-to-application-9 = [ ];
+    };
   };
 }
