a0a10cc44a7b80d73a846db523eedcae20e2663d
maycolmunoz <estivenyeizon@gmail.com>
2026-07-13 20:52:13 -0500

    fix(home-manager): remove cursor size and gtk4 config

--- home/mmunoz/gnome.nix
+++ home/mmunoz/gnome.nix
@@ -105,16 +105,11 @@
     cursorTheme = {
       name = "Bibata-Modern-Classic";
       package = pkgs.bibata-cursors;
-      size = 24;
     };
 
     gtk2.configLocation = "${config.xdg.configHome}/gtk-2.0/gtkrc";
 
     gtk3.extraConfig = {
-      gtk-application-prefer-dark-theme = true;
-    };
-
-    gtk4.extraConfig = {
       gtk-application-prefer-dark-theme = true;
     };
   };
