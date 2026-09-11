d622c608bd1ee73cedbcad0a02f73737d831e34d
maycolmunoz <estivenyeizon@gmail.com>
2026-09-11 09:05:44 -0500

    feat(home): add npm global prefix and session bin path

--- home/mmunoz/programs/dev.nix
+++ home/mmunoz/programs/dev.nix
@@ -1,6 +1,17 @@
-{ pkgs, ... }:
+{ config, pkgs, ... }:
 
 {
+  programs.npm = {
+    enable = true;
+    settings = {
+      prefix = "${config.home.homeDirectory}/.npm-global";
+    };
+  };
+
+  home.sessionPath = [
+    "$HOME/.npm-global/bin"
+  ];
+
   home.packages = with pkgs; [
     php84Packages.composer
 
